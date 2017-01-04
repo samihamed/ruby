7a32b85ee9664aedf46990fa2fda4601dbc4e9d7
Sami Hamed <sami.k.hamed@googlemail.com>
2017-01-04 20:44:04 +0100

    rm calc

--- calc.rb
+++
@@ -1,4 +0,0 @@
-puts "Table of Contents".center 50
-puts "Chapter 1: Numbers".ljust(25) + "Page 1".rjust(25)
-puts "Chapter 2: Letters".ljust(25) + "Page 72".rjust(25)
-puts "Chapter 3: Variables".ljust(25) + "Page 118".rjust(25)
