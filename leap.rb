4fe92e6e150ac69cbcedee1736ca44325e88ecca
Sami Hamed <sami.k.hamed@googlemail.com>
2017-01-04 20:47:47 +0100

    add leap year counter

--- leap.rb
+++ leap.rb
@@ -2,7 +2,7 @@ startyear = gets.chomp
 endyear = gets.chomp
 startyear = startyear.to_i
 endyear = endyear.to_i
-while startyear < endyear
+while startyear <= endyear
   if (startyear % 4 == 0 and startyear % 100 != 0)
     puts startyear
   end
