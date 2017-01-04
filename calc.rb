a22b2538fe5d8c346dfc1340cf03095157b6815b
Sami Hamed <sami.k.hamed@googlemail.com>
2017-01-04 16:24:03 +0100

    First commit with test rb file

---
+++ calc.rb
@@ -0,0 +1,4 @@
+puts "Table of Contents".center 50
+puts "Chapter 1: Numbers".ljust(25) + "Page 1".rjust(25)
+puts "Chapter 2: Letters".ljust(25) + "Page 72".rjust(25)
+puts "Chapter 3: Variables".ljust(25) + "Page 118".rjust(25)
