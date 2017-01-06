9a6a20c2f9965ee6832c8e01f039ab0b85c7756a
Sami Hamed <sami.k.hamed@googlemail.com>
2017-01-06 19:00:26 +0100

    add new ruby project

--- leap.rb
+++ leap.rb
@@ -3,8 +3,14 @@ endyear = gets.chomp
 startyear = startyear.to_i
 endyear = endyear.to_i
 while startyear <= endyear
-  if (startyear % 4 == 0 and startyear % 100 != 0)
-    puts startyear
+  if (startyear % 4 == 0)
+    if (startyear % 100 == 0 and startyear % 400 == 0)
+      puts startyear
+    elsif (startyear % 100 == 0 and startyear % 400 != 0)
+      var = ""
+    else
+      puts startyear
+    end
   end
     startyear += 1
 end
