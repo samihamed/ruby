8e1247784d9d768db2061b795765efb527363a45
Sami Hamed <sami.k.hamed@googlemail.com>
2017-01-08 22:36:28 +0100

    add new ruby project

--- leap.rb
+++ leap.rb
@@ -4,7 +4,7 @@ startyear = startyear.to_i
 endyear = endyear.to_i
 while startyear <= endyear
   if (startyear % 4 == 0)
-    if (startyear % 100 == 0 and startyear % 400 == 0)
+    if (startyear % 100 == 0 and startyear % 400 == 0) #this ensures that years like 1900 aren't counted as leap years
       puts startyear
     elsif (startyear % 100 == 0 and startyear % 400 != 0)
       var = ""
